92c40cc0f7ae0ec178da7f8a0576a3f5325e4006
HyungtaeMoon <blessmht@gmail.com>
2018-11-14 13:24:37 +0900

    [ADD] post_edit 구현

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -7,4 +7,5 @@ app_name = 'blog'
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('<int:pk>/', views.post_detail, name='post_detail'),
+    path('<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
--- app/blog/views.py
+++ app/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, UpdateView
 
 from .models import Post
 
@@ -7,13 +7,4 @@ index = ListView.as_view(model=Post, template_name='blog/index.html')
 
 post_detail = DetailView.as_view(model=Post)
 
-# def post_detail(request, pk):
-#     '''
-#     post_detail의 FBV 이며, Post DB 에서
-#     pk 값에 해당하는 객체만을 불러와서 뿌려준다.
-#     '''
-#     post = Post.objects.filter(pk=pk)
-#     context = {
-#         'post': post,
-#     }
-#     return render(request, 'blog/post_detail.html', context)
+post_edit = UpdateView.as_view(model=Post, fields='__all__')
