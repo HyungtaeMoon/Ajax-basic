8ef7448d580838860241cb5e998706197fd14364
HyungtaeMoon <blessmht@gmail.com>
2018-11-14 13:13:17 +0900

    [ADD] post_detail 구현, get_absolute_url 설정

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- app/blog/views.py
+++ app/blog/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
+from .models import Post
 
-def index(request):
-    return render(request, 'blog/index.html')
+index = ListView.as_view(model=Post, template_name='blog/index.html')
+
+post_detail = DetailView.as_view(model=Post)
+
+# def post_detail(request, pk):
+#     '''
+#     post_detail의 FBV 이며, Post DB 에서
+#     pk 값에 해당하는 객체만을 불러와서 뿌려준다.
+#     '''
+#     post = Post.objects.filter(pk=pk)
+#     context = {
+#         'post': post,
+#     }
+#     return render(request, 'blog/post_detail.html', context)
