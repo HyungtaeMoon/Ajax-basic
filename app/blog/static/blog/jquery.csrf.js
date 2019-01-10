938d554a444c6e17de80f2a9589c7c47c75a50bd
HyungtaeMoon <blessmht@gmail.com>
2019-01-10 13:34:54 +0900

    [FIX] jquery.csrf.js 로드되지 않는 현상 해결 및 기타 문제사항 해결

--- app/blog/static/blog/jquery.csrf.js
+++ app/blog/static/blog/jquery.csrf.js
@@ -18,11 +18,11 @@ var csrftoken = getCookie('csrftoken');
 
 function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
-    return (/^GET|HEAD|OPTION|TRACE)$/.test(method));
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
 
 $.ajaxSetup({
-    beforeSend: function (xhr, settings) {
+    beforeSend: function(xhr, settings) {
         if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
             xhr.setRequestHeader("X-CSRFToken", csrftoken);
         }
