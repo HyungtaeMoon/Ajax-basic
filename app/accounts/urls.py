46c0a476e8b3d29fe7738e991f9d07a872ffcf50
HyungtaeMoon <blessmht@gmail.com>
2019-01-18 21:35:48 +0900

    [ADD] CBV 로 로그인 기능 구현

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('signup/', views.SignupForm.as_view(), name='signup'),
+    # 따로 views.py 에서 정의하지 않고 CBV 로 구현. success_url 은 profile
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
 ]
