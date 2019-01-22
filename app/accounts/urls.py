d1b8aa343531ac070db831541c010f6000dd0a98
HyungtaeMoon <blessmht@gmail.com>
2019-01-22 13:17:32 +0900

    [ADD] Ajax/Modal 로 로그인/회원가입 기능 구현

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -7,8 +7,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', views.SignupForm.as_view(), name='signup'),
-    # 따로 views.py 에서 정의하지 않고 CBV 로 구현. success_url 은 profile
-    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('login/', views.LoginForm.as_view(), name='login', kwargs={'template_name': 'accounts/login.html'}),
     # LogoutView 를 사용하면 리다이렉트 되는 곳은 admin 의 logout 페이지가 사용된다
     #   templates 에서 url 다음에 ?next={{ request.path }} 와 같이 설정 필수
     path('logout/', LogoutView.as_view(), name='logout'),
--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView as AuthLoginView
+from django.http import JsonResponse
+from django.shortcuts import render
 from django.views.generic import CreateView
 
 
@@ -9,6 +11,18 @@ class SignupForm(CreateView):
     form_class = UserCreationForm
     template_name = 'accounts/signup_form.html'
     success_url = settings.LOGIN_URL
+
+    def form_valid(self, form):
+        #
+        response = super().form_valid(form)
+        if self.request.is_ajax():
+            return JsonResponse({'next_url': self.get_success_url()})
+        return response
+
+    def get_template_names(self):
+        if self.request.is_ajax():
+            return ['accounts/_signup_form.html']
+        return ['accounts/signup_form.html']
 
 
 # login_required 데코레이터를 사용하지 않으면,
@@ -18,3 +32,21 @@ def profile(request):
     # login 을 하면서 request 에 유저의 정보가 담겨있기 때문에 render 로
     #   request 와 template 만 지정
     return render(request, 'accounts/profile.html')
+
+
+class LoginForm(AuthLoginView):
+    def form_valid(self, form):
+        if self.request.is_ajax():
+            # POST 요청을 통해 들어온 form 데이터
+            response = super().form_valid(form)
+            if self.request.is_ajax():
+                # 템플릿의 ajax 처리 부분에서 response.next_url 로 데이터를 보냄
+                return JsonResponse({'next_url': self.get_success_url()})
+            return response
+
+    def get_template_names(self):
+        if self.request.is_ajax():
+            # ajax 처리로 들어오면 modal 폼을 띄우고
+            return ['accounts/_login.html']
+        # url 로 get 요청하면 단순 login.html 페이지를 보여줌
+        return ['accounts/login.html']
