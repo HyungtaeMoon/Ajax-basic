a3f6afb988c97d7b4e989eafe2ecba55d1f59e3b
HyungtaeMoon <blessmht@gmail.com>
2019-01-25 10:49:16 +0900

    [ADD] chart 데이터를 ajax 로 공급받음

--- app/mychart/urls.py
+++ app/mychart/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('data.json/', views.data_json, name='data_json'),
 ]
--- app/mychart/views.py
+++ app/mychart/views.py
@@ -1,10 +1,22 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from .utils import get_comic_info
 
 
 def index(request):
+    return render(request, 'mychart/index.html')
+
+
+def data_json(request):
     comic = get_comic_info(183559, '신의 탑')
-    context = {
-        'comic': comic,
-    }
-    return render(request, 'mychart/index.html', context)
+    return JsonResponse({
+        'labels': [ep['title'] for ep in comic['ep_list']],
+        'datasets': [{
+            'label': '평점',
+            'backgroundColor': 'rgba(255, 99, 132, 0.5)',
+            'borderColor': 'rgba(255, 99, 132, 1)',
+            'pointBackgroundColor': 'rgba(255, 99, 132, 1)',
+            'pointBorderColor': '#fff',
+            'data': [ep['rating'] for ep in comic['ep_list']],
+        }],
+    })
