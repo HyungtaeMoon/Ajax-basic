7c076e11778ab2d54c66072fec8e2c675027e1b1
HyungtaeMoon <blessmht@gmail.com>
2019-02-04 15:35:38 +0900

    [COMMENT] 회원가입 관련 주석 설명 추가

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -8,18 +8,24 @@ from django.views.generic import CreateView
 
 
 class SignupForm(CreateView):
+    # UserCreationForm 을 상속받아 password1 과 password2 를 검증
     form_class = UserCreationForm
     template_name = 'accounts/signup_form.html'
     success_url = settings.LOGIN_URL
 
     def form_valid(self, form):
-        #
+        # super() 를 사용하여 form 호출이 한번만 발생하도록 함
+        #   만약 super() 가 없으면 RecursionError: maximum recursion depth exceeded 발생
         response = super().form_valid(form)
+        # form.ajaxSubmit 요청이 올 경우
         if self.request.is_ajax():
+            # 자바스크립트로 처리되기 때문에 json 타입으로 응답을 보내줌
             return JsonResponse({'next_url': self.get_success_url()})
         return response
 
     def get_template_names(self):
+        # ajax 로 들어오는 폼과 get 요청을 통해 그냥 들어오는 form 에 대한
+        #   return 을 각각 다른 템플릿으로 보내줌
         if self.request.is_ajax():
             return ['accounts/_signup_form.html']
         return ['accounts/signup_form.html']
